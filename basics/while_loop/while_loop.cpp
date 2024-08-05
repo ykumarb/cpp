818b0ae7662f896e4be8008a04439e4189e1ad92
root <root@2CR2BW3.synaptics-inc.local>
2024-08-05 17:56:38 +0530

    While loop in c++
    1. Basic while loop
    2. Do while example

---
+++ basics/while_loop/while_loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void) {
+
+	int i = 1;
+
+	while (i <= 5)  {
+		cout << "Yupindra " << i << "\n";
+		i += 1;
+	}
+
+	i = 2;
+	do {
+		cout << "Majestic " << i << "\n";
+		i += 1;
+
+	} while (i <= 1);
+
+	return 0;
+}
